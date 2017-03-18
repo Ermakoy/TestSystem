fdb60edb76e18467bbaf14b2068d54ae0ea5f033
CheshireB <NIkitaUniversity@yandex.ru>
2017-03-19 00:58:18 +0300

    Rename of papok and append ,odels for DB

---
+++ testsystem/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class tasks(models.Model):
+
+    task_number = models.IntegerField ( default=-1 )
+    task = models.CharField ( max_length= 500 )
+    answer = models.CharField ( max_length=20 )
+    subject = models.CharField ( max_length= 20 )
+    id_test = models.IntegerField ()
+    image = models.CharField ( max_length= 500) # link on picture
+
+
+class comment_task (models.Model):
+
+    id_task = models.ForeignKey ( tasks )
+    comment = models.CharField (max_length= 500 )
+
+
+class temp_test (models.Model):
+    id_test = models.IntegerField()
+    id_task = models.ForeignKey( tasks )
+
+
+class suggestion ( models.Model ):
+    task_number = models.IntegerField ( default=-1 )
+    task = models.CharField ( max_length= 1000 )
+    answer = models.CharField ( max_length=20 )
+    subject = models.CharField ( max_length= 20 )
+    comment = models.CharField (max_length= 500 )
+    image = models.CharField( max_length=500 )  # link on picture
+    flag = models.IntegerField ( default= 0 ) # 0 - not seemed; -1 - denied; 1 - accept
