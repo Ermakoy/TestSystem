45ed458d78fe9f4a72942e10a8e29576ca43afd4
CheshireB <NIkitaUniversity@yandex.ru>
2017-03-27 19:11:38 +0300

    DB v1.0.1

--- testsystem/admin.py
+++ testsystem/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 
+class TasksAdmin ( admin.ModelAdmin ):
+
+    list_display_links = ('id',)
+
+    list_display = ('id', 'type_task', 'subject', 'task', 'answer', 'image', 'test_id', 'date_pub', 'flag', )
+
+    list_filter = ('subject', 'type_task')
+
+
+
+
+admin.site.register( tasks, TasksAdmin)
