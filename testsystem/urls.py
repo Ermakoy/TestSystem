fb9561d1a5b4c4d93d5ebd3964af150627fa60d7
CheshireB <NIkitaUniversity@yandex.ru>
2017-03-28 12:58:58 +0300

    Work with Querry set

--- testsystem/urls.py
+++ testsystem/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-url(r'^preview$', views.preview, name='preview'),
+    url(r'^preview$', views.preview, name='preview'),
+    url(r'^get_test$', views.get_test, name='get_test'),
 ]
