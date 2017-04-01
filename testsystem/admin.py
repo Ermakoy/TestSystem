cae039cee5491ec583c913c5a02d3a7920024385
CheshireB <NIkitaUniversity@yandex.ru>
2017-04-01 20:18:56 +0300

    No all is work

--- testsystem/admin.py
+++ testsystem/admin.py
@@ -2,15 +2,19 @@ from django.contrib import admin
 from .models import *
 
 
-class TasksAdmin ( admin.ModelAdmin ):
+class TasksAdmin(admin.ModelAdmin):
 
     list_display_links = ('id',)
 
-    list_display = ('id', 'type_task', 'subject', 'task', 'answer', 'image', 'test_id', 'date_pub', 'flag', )
+    list_display = (
+        'id',        'subject_id',
+        'type_task', 'task',
+        'answer',    'image',
+        'test_id',   'date_pub',
+        'flag',)
 
-    list_filter = ('subject', 'type_task')
 
-
+    list_filter = ('subject_id', 'type_task')
 
 
 admin.site.register( tasks, TasksAdmin)
--- testsystem/models.py
+++ testsystem/models.py
@@ -2,56 +2,54 @@ from django.db import models
 
 
 
-class tasks (models.Model):
+class tasks(models.Model):
 
     type_task = models.IntegerField (verbose_name='Номер задания')
 
+
+    task = models.TextField(verbose_name='Текст задания')
+
+    answer = models.CharField(max_length=20,
+                              verbose_name='Ответ')
+
+    image = models.CharField(blank=True,
+                             max_length=100,
+                             verbose_name='Картинка к заданию')
     subject_choises = (
         ('Math', 'Математика'),
         ('Russian', 'Русскйи язык'),
     )
 
-    subject =  models.CharField (   max_length=20,
-                                    choices=subject_choises,
-                                    default='None',
-                                    verbose_name='Предмет')
+    subject_id =  models.CharField(max_length=20,
+                                   choices=subject_choises,
+                                   default='Math',
+                                   verbose_name='Предмет')
 
-    task = models.TextField(verbose_name='Текст задания')
+    test_id = models.IntegerField(blank=True,
+                                  verbose_name='Номер теста')
 
-    answer = models.CharField ( max_length=20,
-                                verbose_name='Ответ')
-
-    image = models.CharField (  blank=True,
-                                max_length=100,
-                                verbose_name='Картинка к заданию')
-
-    test_id = models.IntegerField (blank=True,
-                                   verbose_name='Номер теста')
-
-    date_pub = models.DateField ( verbose_name='Дата публикации',
-                                  auto_now_add=True )
+    date_pub = models.DateField(verbose_name='Дата публикации',
+                                auto_now_add=True )
 
     flag_choices = (
         ('0', 'Не проверено'),
         ('1', 'Проверено'),
     )
 
-    flag = models.CharField ( max_length=20,
-                              default='1',
-                              choices=flag_choices,
-                              help_text='Поле для проверки задания, проверил задание - отметь единичкой ;)')
+    flag = models.CharField(max_length=20,
+                            default='1',
+                            choices=flag_choices)
 
 
 
 
-class tasks_comments (models.Model):
+class tasks_comments(models.Model):
 
-    id_task = models.ForeignKey  (tasks)
+    id_task = models.ForeignKey(tasks)
+    comment = models.TextField(verbose_name='Комментарий')
 
-    comment = models.TextField ( verbose_name='Комментарий')
 
+class temp_test(models.Model):
 
-class temp_test (models.Model):
-
-    id_test = models.IntegerField( verbose_name='Тест')
-    tasks = models.TextField ( verbose_name='Задания')
+    id_test = models.IntegerField(verbose_name='Тест')
+    tasks = models.TextField(verbose_name='Задания')
