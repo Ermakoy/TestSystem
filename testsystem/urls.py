96c9f2f0266bd7f6152873d5f13fc077dc037301
CheshireB <NIkitaUniversity@yandex.ru>
2017-04-10 23:28:23 +0300

    append multisubj+fix

--- testsystem/urls.py
+++ testsystem/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^math$', views.math),
+    url(r'^(?P<subject>[\w-]+)/$', views.subject),
     url(r'^api/tasks', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^math/get_test/(?P<num>[1-6]+)/$', views.get_test)
+    url(r'^(?P<subs>[\w-]+)/(?P<subject>[\w-]+)/get_test/(?P<num>[1-6]+)/$', views.get_test)
 ]
