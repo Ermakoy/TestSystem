e656a32b7f046cd794fce45882e612e3f7b1a7cd
CheshireB <NIkitaUniversity@yandex.ru>
2017-04-11 11:31:25 +0300

    Append simple API

--- testsystem/urls.py
+++ testsystem/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from . import views
+
 
 urlpatterns = [
     url(r'^(?P<subject>[\w-]+)/$', views.subject),
-    url(r'^api/tasks', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^(?P<subs>[\w-]+)/(?P<subject>[\w-]+)/get_test/(?P<num>[1-6]+)/$', views.get_test)
+    url(r'^(?P<subs>[\w-]+)/(?P<subject>[\w-]+)/get_test/(?P<num>[1-6]+)/$', views.get_test),
+    url(r'^api/tasks/$', views.tasks_list)
 ]
