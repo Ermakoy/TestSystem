37a25acd350a52ba61467ebe01157b110ffd5b2d
CheshireB <NIkitaUniversity@yandex.ru>
2017-04-12 19:22:31 +0300

    Begin of check test static

--- testsystem/urls.py
+++ testsystem/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<subject_t>[\w-]+)/$', views.subject),
-    url(r'^(?P<subject_t>[\w-]+)/get_static_test/(?P<num>[1-6]+)/$', views.get_test, name='get static test'),
+    url(r'^(?P<subject_t>[\w-]+)/get_static_test/(?P<num>[1-6]+)/$', views.get_static_test, name='get static test'),
+    url(r'^(?P<subject_t>[\w-]+)/get_static_test/(?P<num>[1-6]+)/(?P<answer_t>[\w-]+)/$', views.check_static_test),
     url(r'^api/tasks/$', views.tasks_list),
-    url(r'^api/tasks/(?P<pk>[0-9]+)/$', views.tasks_detail)
 ]
