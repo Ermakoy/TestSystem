0265b112c917fc3da2606949c0ecb014e1e39b88
CheshireB <NIkitaUniversity@yandex.ru>
2017-04-13 09:53:59 +0300

    urls+views

--- testsystem/urls.py
+++ testsystem/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^(?P<subject_t>[\w-]+)/$', views.subject),
     url(r'^(?P<subject_t>[\w-]+)/get_static_test/(?P<num>[1-6]+)/$', views.get_static_test, name='get static test'),
     url(r'^(?P<subject_t>[\w-]+)/get_static_test/(?P<num>[1-6]+)/(?P<answer_t>[\w-]+)/$', views.check_static_test),
+    url(r'^(?P<subject_t>[\w-]+)/get_temp_test/^(?P<answer>[\w-]+)/$', views.get_new_temp_test),
     url(r'^api/tasks/$', views.tasks_list),
 ]
--- testsystem/views.py
+++ testsystem/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import render_to_response
+from django.db.models import Max
 
 
 from .models import tasks, Subject
@@ -13,16 +14,23 @@ def subject(request, subject_t):
     args = {}
     args['nameoftasks'] = Subject.objects.filter(subjecteng=subject_t)
     args['subjecteng'] = subject_t;
-    args['xrange'] = ['1','2','3','4','5','6']
+    # Вывод максимального номера статического теста и запись его в args в качестве списка от 1 до n
+    args['xrange'] = [
+        str(i) for i in range(1, tasks.objects.filter(subject_id=subject_t).aggregate(Max('test_id'))['test_id__max']+1)]
     return render_to_response('testsystem/subject.html', args)
 
 def get_static_test(request, subject_t, num):
     args = {}
     args['subject'] = subject_t
-    args['nameoftasks'] = Subject.objects.filter(subjecteng=subject_t)
+    args['nameoftasks'] = Subject.objects.filter(subjecteng=subject_t).order_by('typeoftask')
     args['queryset'] = tasks.objects.filter(test_id=num, subject_id=subject_t)
     return render_to_response('testsystem/test.html', args)
 
+# Задел под создание и выдачи темп тест
+def get_new_temp_test(request, subject_t):
+    args = {}
+
+# Задел на json ответ
 def check_static_test(request, subject_t, num, answer_t):
     queryset = tasks.objects.filter(test_id=num, subject_id=subject_t).order_by('type_task')
     serializer = TestStaticAnswer(tasks, many=True)
